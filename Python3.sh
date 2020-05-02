f0299d71c4fdd1b54d49ca7829d15b73077c049d
swjsky <admin@shikey.com>
2020-05-02 21:26:20 +0800

    Update Python3.sh

--- Python3.sh
+++ Python3.sh
@@ -1,10 +1,9 @@
-yum -y install wget &&
 yum -y groupinstall "Development tools" &&
 yum -y install zlib-devel bzip2-devel openssl-devel ncurses-devel sqlite-devel readline-devel tk-devel gdbm-devel db4-devel libpcap-devel xz-devel &&
-wget https://www.python.org/ftp/python/3.5.5/Python-3.5.5.tar.xz &&
+wget https://www.python.org/ftp/python/3.7.7/Python-3.7.7.tar.xz &&
 mkdir /usr/local/python3 &&
-tar -xvJf  Python-3.5.5.tar.xz &&
-cd Python-3.5.5 &&
+tar -xvJf  Python-3.7.7.tar.xz &&
+cd Python-3.7.7 &&
 ./configure --prefix=/usr/local/python3 &&
 make && make install &&
 ln -s /usr/local/python3/bin/python3 /usr/bin/python3 &&
