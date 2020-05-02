58a74dfe3047122c544ccbbbc7624d0d5e45386f
swjsky <admin@shikey.com>
2020-05-02 21:20:43 +0800

    Update Python3.sh

--- Python3.sh
+++ Python3.sh
@@ -5,7 +5,7 @@ wget https://www.python.org/ftp/python/3.5.5/Python-3.5.5.tar.xz &&
 mkdir /usr/local/python3 &&
 tar -xvJf  Python-3.5.5.tar.xz &&
 cd Python-3.5.5 &&
-./configure --prefix=/usr/local/python3 &&&
+./configure --prefix=/usr/local/python3 &&
 make && make install &&
 ln -s /usr/local/python3/bin/python3 /usr/bin/python3 &&
 ln -s /usr/local/python3/bin/pip3 /usr/bin/pip3 &&
